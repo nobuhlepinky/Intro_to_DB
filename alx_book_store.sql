0b036a54061d3ec56324b1ba68cd04847ac1ebec
Nobuhle_Pinky <nobuhlepinky11@gmail.com>
2025-12-14 16:24:01 +0200

    Initial creation for alx_book_store database

---
+++ alx_book_store.sql
@@ -0,0 +1,50 @@
+
+
+-- 1. CREATE THE DATABASE
+CREATE DATABASE IF NOT EXISTS ALX_BOOK_STORE;
+
+-- 2. USE THE NEWLY CREATED DATABASE
+USE ALX_BOOK_STORE;
+
+-- 3. CREATE THE 'Authors' TABLE
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+-- 4. CREATE THE 'Books' TABLE
+CREATE TABLE IF NOT EXISTS Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+-- 5. CREATE THE 'Customers' TABLE
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
+    address TEXT
+);
+
+-- 6. CREATE THE 'Orders' TABLE
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- 7. CREATE THE 'Order_Details' TABLE (Junction Table)
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
+
