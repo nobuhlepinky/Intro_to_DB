5d09be6f08a9afd4e15e5bf4b9ee42a8063a78c7
Nobuhle_Pinky <nobuhlepinky11@gmail.com>
2025-12-14 18:17:41 +0200

    error fixed

--- task_3.sql
+++ task_3.sql
@@ -1,4 +1,8 @@
 -- FILE: task_3.sql
--- PURPOSE: List all tables in the database provided via the command line argument.
+-- PURPOSE: List all tables in the alx_book_store database.
 
+-- Explicitly select the required database
+USE alx_book_store;
+
+-- List all tables in the selected database
 SHOW TABLES;
